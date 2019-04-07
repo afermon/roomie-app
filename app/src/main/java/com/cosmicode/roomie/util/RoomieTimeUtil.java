7e59dd8d72f5db1961c928fb6c15456f2ac2513d
afermon <alex.fermon@outlook.com>
2019-04-07 14:46:47 -0600

    Roomie time util

--- app/src/main/java/com/cosmicode/roomie/util/RoomieTimeUtil.java
+++ app/src/main/java/com/cosmicode/roomie/util/RoomieTimeUtil.java
@@ -1,45 +1,61 @@
 package com.cosmicode.roomie.util;
 
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.chrono.ISOChronology;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class RoomieTimeUtil {
 
-    private static DateTimeFormatter inputDateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private static DateTimeFormatter instantDateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
             .withLocale(Locale.ROOT)
             .withChronology(ISOChronology.getInstanceUTC());
 
-    private static DateTimeFormatter outputDateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZ").withZoneUTC();
-
-    public static DateTime instantStringToDateTime(String date){
-        return inputDateTimeFormatter.parseDateTime(date);
+    public static DateTime instantUTCStringToDateTime(String date){
+        return instantDateTimeFormatter.parseDateTime(date).withZone(DateTimeZone.forID(TimeZone.getDefault().getID()));
     }
 
-    public static Calendar dateStringToCalendar(String date) {
+    public static String dateTimeToInstantUTCString(DateTime datetime){
+        return instantDateTimeFormatter.print(datetime);
+    }
+
+    public static Calendar instantUTCStringToCalendar(String date) {
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(instantStringToDateTime(date).toDate());
+        calendar.setTime(instantUTCStringToDateTime(date).toDate());
         return calendar;
     }
 
-    public static String formatInstantStringDateTime(String date){
-        return inputDateTimeFormatter.parseDateTime(date).toString("yyyy/MM/dd hh:mm a");
+    public static String instantUTCStringToLocalDateTimeString(String date){
+        return instantUTCStringToDateTime(date).toString("yyyy/MM/dd hh:mm a");
     }
 
-    public static String formatInstantStringDate(String date){
-        return inputDateTimeFormatter.parseDateTime(date).toString("yyyy/MM/dd");
+    public static String instantUTCStringToLocalDateString(String date){
+        return instantUTCStringToDateTime(date).toString("yyyy/MM/dd");
     }
 
-    public static String formatInstantStringTime(String date){
-        return inputDateTimeFormatter.parseDateTime(date).toString("hh:mm a");
+    public static String instantUTCStringToLocalTimeString(String date){
+        return instantUTCStringToDateTime(date).toString("hh:mm a");
     }
 
-    public static String datetimeToInstantString(DateTime datetime){
-        return outputDateTimeFormatter.print(datetime);
+    public static DateTime calendarToDateTime(Calendar calendar){
+        return new DateTime(calendar.getTimeInMillis(), DateTimeZone.forID(calendar.getTimeZone().getID()));
+    }
+
+    public static String calendarToInstantUTCString(Calendar calendar){
+        return dateTimeToInstantUTCString(calendarToDateTime(calendar));
+    }
+
+    public static String calendarToDateString(Calendar calendar){
+        return  instantUTCStringToLocalDateString(calendarToInstantUTCString(calendar));
+    }
+
+    public static String calendarToTimeString(Calendar calendar){
+        return  instantUTCStringToLocalTimeString(calendarToInstantUTCString(calendar));
     }
 
     public static boolean isSameDay(Calendar cal1, Calendar cal2) {
