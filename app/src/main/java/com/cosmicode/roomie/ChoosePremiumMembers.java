a2ea5eb3132ee84c5a807dc91cbe93b5c26914ed
DESKTOP-V7L9RRS\dek_5 <djimenezq@ucenfotec.ac.cr>
2019-04-10 11:51:43 -0600

    premium info activity

---
+++ app/src/main/java/com/cosmicode/roomie/ChoosePremiumMembers.java
@@ -0,0 +1,14 @@
+package com.cosmicode.roomie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoosePremiumMembers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_premium_members);
+    }
+}
---
+++ app/src/main/java/com/cosmicode/roomie/CreatePremiumRoom.java
@@ -0,0 +1,14 @@
+package com.cosmicode.roomie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePremiumRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_premium_room);
+    }
+}
